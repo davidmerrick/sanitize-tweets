ebaaf66435e2acdea3bf0db0ed772acf883592fd
David Merrick <david@david-merrick.com>
2017-06-12 13:11:26 -0700

    Added more filter criteria

--- index.js
+++ index.js
@@ -31,15 +31,30 @@ function sanitizeTweets() {
         filterArray.push(!(tweetText.startsWith(".@") && tweetText.length < 50)); // filter out replies to other Twitter accounts
         filterArray.push(!(tweetText[0] === "\"" && tweetText.indexOf("--" != -1))); // filter out quotations
         filterArray.push(!(tweetText[0] === "“" && tweetText.indexOf("--" != -1))); // filter out quotations
+        filterArray.push(tweetText.match("-- [A-Za-z]+ [A-Za-z]+$") === null); // filter out quotations
+        filterArray.push(tweetText.toLowerCase().match("[^A-Za-z]?tune in[^A-Za-z]") === null); // "tune in"
         filterArray.push(!tweetText.toLowerCase().endsWith("the art of the deal"));
         filterArray.push(!tweetText.toLowerCase().endsWith("midas touch"));
         filterArray.push(!tweetText.toLowerCase().startsWith("rt "));
+        filterArray.push(!tweetText.toLowerCase().startsWith("via "));
         filterArray.push(!tweetText.toLowerCase().startsWith("check out "));
         filterArray.push(!tweetText.toLowerCase().startsWith("my interview "));
+        filterArray.push(!tweetText.toLowerCase().startsWith("i'll be on "));
+        filterArray.push(!tweetText.toLowerCase().startsWith("will be on "));
+        filterArray.push(!tweetText.toLowerCase().startsWith("entrepreneurs:"));
+        filterArray.push(!tweetText.toLowerCase().startsWith("i will be on "));
         filterArray.push(!tweetText.toLowerCase().startsWith("from donald trump: "));
         filterArray.push(!tweetText.toLowerCase().startsWith("donald trump appear"));
         filterArray.push(!(tweetText.toLowerCase().indexOf("my new book") != -1));
+        filterArray.push(!(tweetText.toLowerCase().indexOf("premiere") != -1));
+        filterArray.push(!(tweetText.toLowerCase().indexOf("finale") != -1));
+        filterArray.push(!(tweetText.toLowerCase().indexOf(" will be on ") != -1));
+        filterArray.push(!(tweetText.toLowerCase().indexOf("#tbt") != -1));
         filterArray.push(!(tweetText.toLowerCase().indexOf("albert einstein") != -1));
+        filterArray.push(!(tweetText.toLowerCase().indexOf("happy birthday") != -1));
+        filterArray.push(!(tweetText.toLowerCase().indexOf(" via @") != -1));
+        filterArray.push(!(tweetText.toLowerCase().indexOf("interview") != -1 && tweetText.toLowerCase().indexOf("discussing") != -1));
+        filterArray.push(!(tweetText.toLowerCase().indexOf("int.") != -1 && tweetText.toLowerCase().indexOf("discussing") != -1));
         filterArray.push(!(tweetText.toLowerCase().indexOf("winston churchill") != -1));
         filterArray.push(!(tweetText.toLowerCase().indexOf("thank you") != -1 && tweetText.toLowerCase().indexOf("#supertuesday") != -1));
         return !filterArray.includes(false);
